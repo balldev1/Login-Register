156f7633995d98666b17e22a1f37557fcd63ca3b
balldev1 <balldev1@hotmail.com>
2023-07-16 10:39:28 +0700

    first commit

---
+++ login/app/page.jsx
@@ -0,0 +1,10 @@
+import Button from '@mui/material/Button';
+import Login from './login/page';
+
+export default function Home() {
+  return (
+    <div className='flex justify-center mt-5'>
+      <Login />
+    </div >
+  )
+}
